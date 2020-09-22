7f3020ca1d5c5aa8dbbd805b7613ad983298d83b
aleksandr <apastukhanov@mail.ru>
2020-09-23 01:08:14 +0300

    refactored some funcs and added more emoji

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-"""Сервер Telegram бота, запускаемый непосредственно"""
+"""Сервер для бота, запускаем и учим английские слова в Телеге"""
 import logging
 import os
 
 from aiogram import Bot, Dispatcher, executor, types
-
 from aiogram.types import CallbackQuery
 
 from words_gen import gen_message_string_with_random_words, gen_quiz
@@ -16,69 +15,133 @@ dp = Dispatcher(bot)
 
 quiz_controller_dict = {}
 
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    """Отправляет приветственное сообщение и помощь по боту"""
+    """Отправляет приветствие и помощь по боту"""
     await message.answer(
-        "Бот для изучения английских слов\n\n"
+        "<u><b>Бот для изучения английских слов</b></u>\n\n"
+        "<i>Список доступных команд</i>\n\n"
         "Приcлать 5 рандомных слов: /words\n\n"
         "Начать квиз: /quiz\n\n"
         "Завершить квиз: /stop_quiz\n\n"
+        "Помощь по боту: /help\n\n"
+        "Запустить бота: /start",
+        parse_mode=types.ParseMode.HTML
         )
 
 
 @dp.message_handler(commands=['words'])
 async def send_words(message: types.Message):
-    """Отправляет 5 слов"""
+    """Отправляем 5 случайный слов выбранных из словаря"""
     words_gen = gen_message_string_with_random_words()
     await message.answer(words_gen, parse_mode=types.ParseMode.HTML)
+
 
 @dp.message_handler(commands=['stop_quiz'])
 async def stop_quiz(message: types.Message):
     """Останавливаем quiz"""
-    await message.answer("Окей! Квиз остановлен!")
+    try:
+        if quiz_controller_dict[message.chat.id]["quiz_started"]:
+            quiz_controller_dict[message.chat.id]["quiz_started"]=False
+            await message.answer("Окей, Quiz остановлен!")
+            await print_quiz_results(message)
+        else:
+            await message.answer("Quiz уже давно не работает &#128579",parse_mode=types.ParseMode.HTML)
+    except:
+        await message.answer("Quiz еще не запускали... &#128579",parse_mode=types.ParseMode.HTML)
+    
 
 @dp.message_handler(commands=['quiz'])
 async def start_quiz(message: types.Message):
-    """Отправляет предложение пройти квиз"""
-    quiz_controller_dict.update({message.chat.id:{"attempt":0,"right_ans":0}})
+    """Запускаем счетчики для quiz"""
+    quiz_controller_dict.update({
+        message.chat.id:
+            {"attempt":0,
+            "right_ans":0,
+            "quiz_started":True}
+        })
+
+    await message.answer("<b>Добро пожаловать в квиз!\n"
+                    "3..2..1.. Поехали.. </b> 🚀🚀🚀", 
+                parse_mode=types.ParseMode.HTML)
+
     await send_quiz(message)
 
-@dp.message_handler(commands=['quiz2'])
-async def send_quiz(message: types.Message):   
+
+@dp.message_handler()
+async def send_quiz(message: types.Message):
+    """Отправляем quiz"""
+
     mess_string, quiz_list, write_answer_value = gen_quiz()
     markup = types.InlineKeyboardMarkup()
-    right_awnswer = lambda answer_str: "верно" if answer_str == write_answer_value else "не верно"
+
+    right_awnswer = lambda answer_str: "верно" \
+    if answer_str == write_answer_value else "не верно"
 
     for var_btn in quiz_list:
-        markup.add(types.InlineKeyboardButton(var_btn, callback_data=right_awnswer(var_btn)))
+        markup.add(
+            types.InlineKeyboardButton(var_btn, 
+            callback_data=right_awnswer(var_btn)))
 
-    await message.answer(mess_string,parse_mode=types.ParseMode.HTML,reply_markup=markup)
+    await message.answer(
+        mess_string,
+        parse_mode=types.ParseMode.HTML,
+        reply_markup=markup)
+
 
 @dp.callback_query_handler(lambda c: c.data == 'верно')
 async def right_answer(callback_query: CallbackQuery):
+    """Поздравляем с правильным ответом на вопрос из quiz"""
+    try:
+        if quiz_controller_dict[callback_query.message.chat.id]["quiz_started"]:
+            quiz_controller_dict[callback_query.message.chat.id]["attempt"]+=1
+            quiz_controller_dict[callback_query.message.chat.id]["right_ans"]+=1
+            
+            await bot.send_message(callback_query.from_user.id, 
+                                        'Правильно &#127881;&#127881;&#127881;',
+                                        parse_mode=types.ParseMode.HTML)
 
-    if quiz_controller_dict[callback_query.message.chat.id]["attempt"] < 5:
-        quiz_controller_dict[callback_query.message.chat.id]["right_ans"]+=1
-        #await bot.answer_callback_query(callback_query.id)
-        await bot.send_message(callback_query.from_user.id, 'Right answer &#127881;&#127881;&#127881;',parse_mode=types.ParseMode.HTML)
-        await send_quiz(callback_query.message)
-    else:
-        await bot.send_message(callback_query.from_user.id, 'Right answer &#127881;&#127881;&#127881;',parse_mode=types.ParseMode.HTML)
-        await bot.send_message(callback_query.from_user.id,"Окей... на сегодня достаточно!\n\nПравильных ответов {} из 5!".format(quiz_controller_dict[callback_query.message.chat.id]["right_ans"]))
-    quiz_controller_dict[callback_query.message.chat.id]["attempt"]+=1
+        if quiz_controller_dict[callback_query.message.chat.id]["attempt"] < 5:
+            await send_quiz(callback_query.message)
+        else:
+            await print_quiz_results(callback_query.message)
+    except KeyError:
+        await start_quiz(callback_query.message)
 
+    
 @dp.callback_query_handler(lambda c: c.data == 'не верно')
 async def wrong_answer(callback_query: CallbackQuery):
+    """Уведомляем об ошибке, если ответ на вопрос из quiz неверный"""
+    try:
+        if quiz_controller_dict[callback_query.message.chat.id]["quiz_started"]:
+            quiz_controller_dict[callback_query.message.chat.id]["attempt"]+=1
+            await bot.send_message(callback_query.from_user.id, 
+                                'Ошибка &#129398!',
+                                parse_mode=types.ParseMode.HTML)
 
-    if quiz_controller_dict[callback_query.message.chat.id]["attempt"] < 5:
-        #await bot.answer_callback_query(callback_query.id)
-        await bot.send_message(callback_query.from_user.id, 'Wrong answer &#9785;',parse_mode=types.ParseMode.HTML)
-        await send_quiz(callback_query.message)
-    else:
-        await bot.send_message(callback_query.from_user.id, 'Wrong answer &#9785;',parse_mode=types.ParseMode.HTML)
-        await bot.send_message(callback_query.from_user.id,"Окей... на сегодня достаточно!\n\nПравильных ответов: {} из 5!".format(quiz_controller_dict[callback_query.message.chat.id]["right_ans"]))
-    quiz_controller_dict[callback_query.message.chat.id]["attempt"]+=1
+        if quiz_controller_dict[callback_query.message.chat.id]["attempt"] < 5:
+            await send_quiz(callback_query.message)
+        else:
+            await print_quiz_results(callback_query.message)
+    except KeyError:
+        await start_quiz(callback_query.message)
+
+
+@dp.callback_query_handler()
+async def print_quiz_results(message: types.Message):
+    try:
+        quiz_controller_dict[message.chat.id]["quiz_started"]=False
+        await message.answer(
+                            "Окей... на сегодня достаточно &#129299\n\n"
+                            "Правильных ответов: <b>{}</b> из <b>{}</b>!\n\n"
+                            "<b>Quiz всегда можно пройти заново\n"
+                            "просто нажми на /quiz</b>".format(
+                                quiz_controller_dict[message.chat.id]["right_ans"],
+                                quiz_controller_dict[message.chat.id]["attempt"]),
+                                parse_mode=types.ParseMode.HTML)
+    except KeyError:
+        await message.answer("Упс.. Ваш quiz куда пропал...&#129325")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- words_gen.py
+++ words_gen.py
@@ -3,16 +3,18 @@ import numpy as np
 import random
 
 def get_random_int_seq(max_size:int, seq_size:int):
+    """Хелпер для генерации выборки с кастомными параметрами"""
     return np.random.randint(0,max_size, size=(1,seq_size))[0]
 
-
 def load_json_data_from_file(file_path: str):
+    """Читаем json"""
     with open(file_path, "r") as j:
-        j_cont = json.load(j)
-    return json.loads(j_cont)
-
+        json_content = json.load(j)
+    return json.loads(json_content)
 
 def gen_random_words(number_of_words:int):
+    """Главная функция-генератор слов на день и для quiz
+       Выбирает рандомные слова и их значения из словаря """
     global j_cont
     seq_random = get_random_int_seq(len(j_cont)-1,number_of_words)
     result = {}
@@ -22,13 +24,15 @@ def gen_random_words(number_of_words:int):
     return result
 
 def gen_message_string_with_random_words():
+    """Генерим сообщение пользователю с 5 словами для изучения"""
     words_dict = gen_random_words(5)
     mess_string = "<i><b>Words for today:</b></i> &#128214\n\n"
     for k in words_dict.keys():
-        mess_string=mess_string+"&#x25B6<u>{}</u>:\n- {}\n\n".format(k,words_dict[k] )
+        mess_string = mess_string + "&#x25B6<u>{}</u>:\n- {}\n\n".format(k,words_dict[k] )
     return mess_string
 
 def gen_quiz():
+    """Генерим quiz для пользователя"""
     words_dict = gen_random_words(4)
     write_answer_key = list(words_dict.keys())[0]
     write_answer_value = words_dict[write_answer_key]
@@ -40,6 +44,5 @@ def gen_quiz():
 j_cont=load_json_data_from_file("data/words_with_definitions.json")
 
 if __name__ == "__main__":
-    #print(gen_message_string_with_random_words())
     mess_string, l, write_answer_key= gen_quiz()
     print(l)
